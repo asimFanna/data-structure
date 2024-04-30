7f2a80a31de7196fee171f76551a49cb76b56ab2
Asim Mohammed <asim@aqar.fm>
2024-04-30 18:46:39 +0300

    NumericTimeSeriesImpl

---
+++ impl/NumericTimeSeriesImpl.java
@@ -0,0 +1,39 @@
+package impl;
+
+import interfaces.DataPoint;
+import interfaces.NumericTimeSeries;
+import main.Helpers;
+
+public class NumericTimeSeriesImpl extends TimeSeriesImp<Double> implements NumericTimeSeries {
+
+    public DLLCompImp<Double> movingAverageList = new DLLCompImp<>();
+
+    @Override
+    public NumericTimeSeries calculateMovingAverage(int period) {
+        dataPointsList.findFirst();
+        for (int i = 0; i < period; i++) {
+            Double value = 0.0;
+            for (int j = 0; j < period; j++) {
+                value += dataPointsList.retrieve().value;
+                if (j != period - 1) dataPointsList.findNext();
+            }
+            value /=period;
+            movingAverageList.insert(value);
+            movingAverageList.sort(true);
+            dataPointsList.findPrevious();
+        }
+
+        Helpers.sortDataPoints(true, dataPointsList.head);
+        return this;
+    }
+
+    @Override
+    public DataPoint<Double> getMax() {
+        return dataPointsList.current.value;
+    }
+
+    @Override
+    public DataPoint<Double> getMin() {
+        return dataPointsList.head.value;
+    }
+}
